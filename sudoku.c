3d26c2150802edda711f54f743511dc3b38e920b
repl.it user <replituser@example.com>
2020-10-17 02:23:39 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -135,12 +135,11 @@ List* get_adj_nodes(Node* n){
 
 
 int is_final(Node* n){
-  
   int i,j;
   for(i=0;i<9;i++){
     for(j=0;j<9;j++){
       printf("%d\n",n->sudo[i][j]);
-      if(n->sudo[i][j]!=0){
+      if(n->sudo[i][j]==0){
         return 0;
         }
       }
