7184524ec5c626cd82e12ed7dd9cbc34bb943427
repl.it user <replituser@example.com>
2020-10-18 03:16:50 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -70,10 +70,10 @@ int is_valid(Node* n){
   int y[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   for(i=0;i<9;i++){
     for(j=0;j<9;j++){
-      if ((x[n->sudo[i][j]]-1 != 0)) {
+      if ((x[n->sudo[i][j]] != 0)) {
         return 0;
       }else{
-        x[n->sudo[i][j] - 1] = 1;
+        x[n->sudo[i][j]] = 1;
       }
       
     }  
